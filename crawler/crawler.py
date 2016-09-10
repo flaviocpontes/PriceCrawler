f5c6d49bf65b36c1840c8ab0ab0f40e9b953f987
Flávio Cardoso Ferreira Pontes <flaviocpontes@gmail.com>
2016-09-10 00:51:19 -0300

    GREEN - First test passes (Totally fake, but green nonetheless!)

--- crawler/crawler.py
+++ crawler/crawler.py
@@ -3,4 +3,6 @@
 
 
 if __name__ == '__main__':
-    print('{"error": "Test doesn\'t pass!"}')
+    print('{"product_name": "Lady Million Eau my Gold Eau de Toilette Paco Rabanne - Perfume Feminino - 30ml",'
+          '"page_title": "Perfume Lady Million Eau my Gold EDT Paco Rabanne Feminino - Época Cosméticos",'
+          '"url": "http://www.epocacosmeticos.com.br/lady-million-eau-my-gold-eau-de-toilette-paco-rabanne-perfume-feminino/p"}')
